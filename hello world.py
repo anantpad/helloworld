4e68b800960f2540f2ffdf3c402523a4e85af83a
Sridhar <srid_24@hotmail.com>
2019-08-15 15:18:59 -0700

    added helloworld.py

---
+++ hello world.py
@@ -0,0 +1,19 @@
+"""Type Hello World
+x=("hello world")
+print(x)"""
+
+"""input("what is your name?")
+print("It's nice to meet you")
+"""
+"""ask the user fortheir name, print the greeting that includes the name"""
+
+"""x=input("What is your Name ?")
+print ("welcome "+x)"""
+
+
+"""print("Rashmi")"""
+
+y=input("What is your name")
+x=input("How are you")
+print("what is your name "+x)
+
